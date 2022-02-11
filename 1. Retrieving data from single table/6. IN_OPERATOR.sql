1dc55637ef894f3052a2d8b37a16000b29dceb07
Tushar Annam <tushar.annam@gmail.com>
2022-02-11 10:43:18 +0530

    Update 6. IN_OPERATOR.sql

--- 1. Retrieving data from single table/6. IN_OPERATOR.sql
+++ 1. Retrieving data from single table/6. IN_OPERATOR.sql
@@ -1,7 +1,7 @@
 USE sql_store;
 SELECT *
 FROM products
-WHERE quantity_in_stock IN (49,38,72,90)
+WHERE quantity_in_stock IN (49,38,72,90)  -- can also be string values
 -- we can also use NOT IN which works opposite to IN operator. 
 -- show products where quantity_in_stock equal to 49 or 38 or 72 or 90.
 -- IN operator is used when we want to fetch for more than one value for a single column.
