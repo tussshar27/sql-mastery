257ca00228cf9f1145df797e2fea548099d1ae34
Tushar Annam <tushar.annam@gmail.com>
2022-02-11 11:39:10 +0530

    Create 5. ORDER_BY_CLAUSE.sql

---
+++ 4. Summarizing Data/5. ORDER_BY_CLAUSE.sql
@@ -0,0 +1,6 @@
+SELECT first_name, last_name, salary
+FROM employees
+ORDER BY salary
+-- ORDER BY clause is used to sort rows in ascending or descending order using specific column(s).
+-- by default, its asc order, DESC keyword is used to convert into descending order.
+-- eg. ORDER BY salary DESC;
