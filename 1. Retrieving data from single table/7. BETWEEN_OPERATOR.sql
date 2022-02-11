6ae8e8d6db8cd225e5aa46fc0917ba7c7fc862ae
Tushar Annam <tushar.annam@gmail.com>
2022-02-11 10:59:11 +0530

    Update 7. BETWEEN_OPERATOR.sql

--- 1. Retrieving data from single table/7. BETWEEN_OPERATOR.sql
+++ 1. Retrieving data from single table/7. BETWEEN_OPERATOR.sql
@@ -2,6 +2,10 @@ USE sql_store;
 SELECT *
 FROM customers
 WHERE birth_date BETWEEN '1990-01-01' AND '2000-01-01'	-- note: date is always consider as String
+-- WHERE birth_date TO_DATE('2020-JAN-15', 'YYYY-MON-DD');  -- TO_DATE function is used to convert String to date with specified date format.
+-- SELECT to_date('20200526','YYYYMMDD');
+-- SELECT TO_DATE('2020-05-26 13:27:18', 'YYYY-MM-DD HH24:MI:SS');
+
 -- WHERE customer_id BETWEEN 5 AND 10
 
 -- WHERE points >= 1000 AND points <= 3000
