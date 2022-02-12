3c68fbb6b8d3dbb8cd1869a667ef4dd0633b3f8f
Tushar Annam <tushar.annam@gmail.com>
2022-02-12 17:31:07 +0530

    Update 4. FORMATTING_DATE_&_TIME.sql

--- 6. Essential MySQL Functions/4. FORMATTING_DATE_&_TIME.sql
+++ 6. Essential MySQL Functions/4. FORMATTING_DATE_&_TIME.sql
@@ -4,3 +4,7 @@ SELECT TIME_FORMAT(NOW(), '%h:%i:%p');	-- 10 43 AM
 -- Above functions are used in MySQL, in Oracle we use TO_CHAR() function.
 
 SELECT TO_CHAR(dateofbirth_col, 'DD/MON/YYYY') FROM employees
+
+SELECT fname, dateofbirth_col FROM employees WHERE TO_CHAR(dateofbirth_col, 'MON') = 'MAR'
+-- Above statement is used to filter date column in which we need all the records
+-- whose dob is march month, soo it will give all the march month records only.
