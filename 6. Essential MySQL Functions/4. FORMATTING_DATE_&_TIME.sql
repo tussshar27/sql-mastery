245a3e7d0156fd963f9579e01b18605363aa7142
Tushar Annam <tushar.annam@gmail.com>
2022-02-12 17:26:56 +0530

    Update 4. FORMATTING_DATE_&_TIME.sql

--- 6. Essential MySQL Functions/4. FORMATTING_DATE_&_TIME.sql
+++ 6. Essential MySQL Functions/4. FORMATTING_DATE_&_TIME.sql
@@ -1,3 +1,6 @@
 SELECT DATE_FORMAT(NOW(), '%m %d %y');	-- 12 30 20
 SELECT DATE_FORMAT(NOW(), '%M %D %Y');	-- December 30th 2020
 SELECT TIME_FORMAT(NOW(), '%h:%i:%p');	-- 10 43 AM
+-- Above functions are used in MySQL, in Oracle we use TO_CHAR() function.
+
+SELECT TO_CHAR(dateofbirth_col, 'DD/MON/YYYY') FROM employees
